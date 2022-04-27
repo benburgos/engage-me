fd8835734b8aca05e19c02e355e563cd2c7cea72
Ben <ben.a.burgos@gmail.com>
2022-04-26 19:07:56 -0700

    heroku deployment pt2

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.get('/', (req, res) => {
 });
 
 // Listener
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {console.log(`You're connected on port ${PORT}!`)});
